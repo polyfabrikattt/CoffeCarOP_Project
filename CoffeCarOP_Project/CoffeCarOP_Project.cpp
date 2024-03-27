490f76ae5ecae18431b44f73e787cd6119ebb1bf
Артур Валентей <valentejvioletta@gmail.com>
2024-03-27 18:21:14 +0300

    Add project files.

---
+++ CoffeCarOP_Project/CoffeCarOP_Project.cpp
@@ -0,0 +1,31 @@
+
+
+#include <iostream>
+using namespace std;
+class coffe {
+public:
+	string name;
+	double price;
+	string moc; //model of coffe(arabic,french)
+	string cupple;
+
+};
+
+
+int main(void)
+{
+	coffe coffe1;
+
+	cout << "input name of coffe(kapuchino,americano and etc)";
+	cin >> coffe1.name;
+
+	cout << "input name of coffe(kapuchino,americano and etc)";
+	cin >> coffe1.name;
+
+	cout << "input name of coffe(kapuchino,americano and etc)";
+	cin >> coffe1.name;
+
+	cout << "input name of coffe(kapuchino,americano and etc)";
+	cin >> coffe1.name;
+   
+}

d6209d068777233620ffd72c0f7d084324083216
Артур Валентей <valentejvioletta@gmail.com>
2024-03-27 18:25:33 +0300

    Добавили новые поля клсса

--- CoffeCarOP_Project/CoffeCarOP_Project.cpp
+++ CoffeCarOP_Project/CoffeCarOP_Project.cpp
@@ -19,13 +19,13 @@ int main(void)
 	cout << "input name of coffe(kapuchino,americano and etc)";
 	cin >> coffe1.name;
 
-	cout << "input name of coffe(kapuchino,americano and etc)";
-	cin >> coffe1.name;
+	cout << "input price of coffe";
+	cin >> coffe1.price;
 
-	cout << "input name of coffe(kapuchino,americano and etc)";
-	cin >> coffe1.name;
+	cout << "input Model of coffe(arabic,UAR,USA and etc)";
+	cin >> coffe1.moc;
 
-	cout << "input name of coffe(kapuchino,americano and etc)";
-	cin >> coffe1.name;
-   
+	cout << "input what cupple of coffe you need(paper,plastic,wafel and etc)";
+	cin >> coffe1.cupple;
+
 }
